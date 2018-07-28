4a24697a6d03bf83bfa2d1124c07439d4a743407
Ryan Gregg <rgregg@users.noreply.github.com>
2018-07-27 17:17:09 -0700

    Move code into main repo

---
+++ OSS/markdowndeep/MarkdownDeepTests/OrderedListTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using MarkdownDeep;
+using System.Reflection;
+
+namespace MarkdownDeepTests
+{
+    [TestFixture]
+    class OrderedListTests
+    {
+
+
+        public static IEnumerable<TestCaseData> GetTests()
+        {
+            return Utils.GetTests("orderedlist");
+        }
+
+        [Test, TestCaseSource("GetTests")]
+        public void Test_orderedlists(string resourceName)
+        {
+            Utils.RunResourceTest(resourceName);
+        }
+    }
+}
